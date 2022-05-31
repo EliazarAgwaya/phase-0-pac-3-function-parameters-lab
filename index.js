41974d4db2af9e4e5ee8e220fc4de34b36d3c130
Eliazar Agwaya <eliazar.magero@student.moringaschool.com>
2022-05-31 21:32:32 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("Eliazar"));
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguage("Eliazar", "JavaScript"));
+
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguageOptional("Eliazar"))
